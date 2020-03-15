366f61c376a4b28bef03823c620783e12372c370
chvdr <chvdr@abv.bg>
2020-03-15 12:20:46 +0200

    Create program stars.py seven times seven stars in a row Changes to be committed: 	modified:   IntroToProgramming/Notes.txt 	new file:   IntroToProgramming/stars.py
    1. a new head unifies all my scripts
    2. the program stars is been created by using (proactively) for loops

---
+++ IntroToProgramming/stars.py
@@ -0,0 +1,13 @@
+# Program: stars.py
+# Author: Chavdar Georgiev
+# Last modified: Sun, Mar 15, 2020 12:12:43 PM
+
+ch = '*'    # The symbol that will be iteratively printed
+n = 7       # This is how many times printing will be iterated
+
+# -- main program code --
+
+for i in range(n):
+    for j in range(n):
+        print(ch, end='')
+    print()
