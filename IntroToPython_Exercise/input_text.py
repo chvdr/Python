8130c9908d087a6366c1f26d3f856bf382942d30
chvdr <chvdr@abv.bg>
2020-03-15 19:46:18 +0200

    Intro exersises

---
+++ IntroToPython_Exercise/input_text.py
@@ -0,0 +1,9 @@
+# Program:
+# Author: Chavdar Georgiev
+# Last modified:
+
+home_town = input('Please tell me where are you from? ')
+
+print("I am from", home_town)
+
+print(f"I am from {home_town}!")
---
+++ IntroToPython_Exercise/read_integer_input.py
@@ -0,0 +1,8 @@
+# Program: Calculate square area by length of the side, given by console
+# Author: Chavdar Georgiev
+# Last modified:
+
+a = int(input('a='))
+s = (a * a)
+
+print(f"Square area = {s}")
